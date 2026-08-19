81e13b002db2511af9dd2a4fa512b55e702aa83a
benjaaamin0518 <nishikawa.1101.sub@gmail.com>
2026-08-19 14:47:48 +0000

    fix: serve queue manager from its public path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: "/queue/",
   server:{
-    port:5173
+    port:5173,
+    allowedHosts: ["digmap.shitai.co.uk"]
   }
 })
